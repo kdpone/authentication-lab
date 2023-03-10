995f0bc211ed39fdf917323a8ac8962bd44cefeb
kdpone <pausangklyde@gmail.com>
2023-03-09 19:06:10 -0700

    fin

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from rest_framework.authtoken.admin import TokenAdmin
+
+TokenAdmin.raw_id_fields = ('user',)
+
+#http POST http://127.0.0.1:8000/api/snippets/ "Authorization:Token 9396679866723e0177fb88c8029f21c5b90ce1e1" code="print('Token works')"
